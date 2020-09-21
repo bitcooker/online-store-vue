95ddf10ff87a59bdee6f75deace47f32aaf3b8a6
bitcooker <dreamhighpassionhigh@gmail.com>
2020-09-21 16:02:09 +0100

    nav bar added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,76 @@
 <template>
 
 <div>
-  <!-- <div>
-        <p v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Mens</p>
-        <p v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Womans</p>
-        <p v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">Kids</p>
-  </div> -->
+ <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Golden Shoe</b-navbar-brand>
 
-  <div>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+     
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="md" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="md" class="m-md-2" type="submit">Search</b-button>
+        </b-nav-form>
+
+       
+
+        <b-dropdown id="dropdown-1" text="Mens" class="m-md-2">
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">All Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Brogues</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Loafers</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Slippers</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Trainers</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Wide Fit Shoes</b-dropdown-item>
+        </b-dropdown>
+
+         <b-dropdown id="dropdown-1" text="Womans" class="m-md-2">
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">All Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Ballet Pumps</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Flat Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Heels</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Trainers</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Wide Fit Shoes</b-dropdown-item>
+        </b-dropdown>
+
+         <b-dropdown id="dropdown-1" text="Kids" class="m-md-2">
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">All Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">School Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">Trainers</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">Wellies</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">Toddler Shoes</b-dropdown-item>
+          <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('kids')" href="#">Canvas</b-dropdown-item>
+        </b-dropdown>
+
+
+         <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+
+            
+
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+  <!-- <div>
   <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
     <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('mens')" href="#">Mens</b-dropdown-item>
     <b-dropdown-item v-on:click="selectedPage('products'), selectedProduct('womans')" href="#">Womans</b-dropdown-item>
@@ -16,7 +79,7 @@
     <b-dropdown-item active>Active action</b-dropdown-item>
     <b-dropdown-item disabled>Disabled action</b-dropdown-item>
   </b-dropdown>
-</div>
+</div> -->
 
 
 </div>
