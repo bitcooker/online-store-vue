ce3dec9ebc45beb24e1d64feac1dd6de66bafd1b
bitcooker <dreamhighpassionhigh@gmail.com>
2020-09-23 15:08:11 +0100

    size chart added

---
+++ src/components/sizeChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum perspiciatis esse magnam enim dicta quaerat impedit? Eligendi excepturi, expedita voluptatum beatae dolores voluptatem modi doloremque facere odio deleniti fugiat quo.</p>
+  </div>
+</template>
+
+<script>
+
+import { eventBus } from '../main';
+
+export default {
+    name: "sizeChart",
+
+      methods: {
+    
+
+      selectedPage(page) {
+        eventBus.$emit("selected-page", page);
+      },
+
+}
+}
+</script>
